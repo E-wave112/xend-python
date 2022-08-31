be038a50592e40947ae201644b68310d9079405c
E-wave <62189260+E-wave112@users.noreply.github.com>
2022-08-31 16:50:14 +0000

    Dev (#23)
    * added the test suite for the esusu strategy
    
    * added code coverage
    
    * added pytest dependency
    
    * added pytest dependency in the requirements.txr file
    
    * added the remaining test cases

--- tests/individual_test.py
+++ tests/individual_test.py
@@ -1,1 +1,57 @@
 # from xend_finance.strategies.individual.individual_index import Personal
+from .main_test import xend
+
+
+def test_flexible_deposit():
+    try:
+        deposit_amount = "20"
+        flexible_deposit = xend.personal.flexible_deposit(deposit_amount)
+        assert type(deposit_amount) == str
+        assert type(flexible_deposit["data"]) == str
+    except Exception as e:
+        assert isinstance(e, Exception)
+
+
+def test_fixed_deposit():
+    try:
+        deposit_amount = "30"
+        lock_period = 4000
+        fixed_deposit = xend.personal.fixed_deposit(deposit_amount, lock_period)
+        assert type(deposit_amount) == str
+        assert type(fixed_deposit["data"]) == str
+    except Exception as e:
+        assert isinstance(e, Exception)
+
+
+def test_get_flexible_deposit_record():
+    try:
+        flexible_deposit = xend.personal.get_flexible_deposit_record()
+        assert type(flexible_deposit["data"]) == list or dict
+    except Exception as e:
+        assert isinstance(e, Exception)
+
+
+def test_get_fixed_deposit_record():
+    try:
+        fixed_deposit = xend.personal.get_fixed_deposit_record()
+        assert type(fixed_deposit["data"]) == list or dict
+    except Exception as e:
+        assert isinstance(e, Exception)
+
+
+def test_get_fixed_withdrawal():
+    try:
+        record_id = 1
+        fixed_withdrawal = xend.personal.fixed_withdrawal(record_id)
+        assert type(fixed_withdrawal["data"]) == str
+    except Exception as e:
+        assert isinstance(e, Exception)
+
+
+def test_get_flexible_withdrawal():
+    try:
+        amount = "20"
+        flexible_withdrawal = xend.personal.flexible_withdrawal(amount)
+        assert type(flexible_withdrawal["data"]) == str
+    except Exception as e:
+        assert isinstance(e, Exception)
